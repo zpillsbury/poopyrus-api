b77e28dcc6e86aa6c9c83c2fdff23966be0d2ea2
Zpillsbury <zpillbury98@gmail.com>
2024-10-30 10:25:15 -0400

    added valid create test

--- app/routers/logs/test_logs.py
+++ app/routers/logs/test_logs.py
@@ -1,6 +1,8 @@
 import pytest
 from httpx import AsyncClient
 
+from app.settings import settings
+
 pytestmark = pytest.mark.asyncio
 AUTH_HEADER = {"Authorization": "Bearer GOOD_TOKEN"}
 
@@ -23,3 +25,14 @@ async def test_create_log(test_client: AsyncClient) -> None:
         headers={"Authorization": "Bearer BAD_TOKEN"},
     )
     assert r.status_code == 401
+
+    # Valid Create
+    r = await test_client.post(
+        "/v1/logs",
+        json={"name": "string", "type": "string", "date": "2024-10-30T13:52:23.666Z"},
+        headers={"Authorization": f"Bearer {settings.static_token}"},
+    )
+    assert r.status_code == 200
+
+    results = r.json()
+    assert results.get("id")
